4a0e7f0202f2c010149d7f548eec0cdcd9c2fdfc
haylie-pitt <htc8@pitt.edu>
2024-11-24 22:50:39 -0500

    arthur is the best

---
+++ QAErrorTracker/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace QAErrorTracker;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ QAErrorTracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace QAErrorTracker;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
